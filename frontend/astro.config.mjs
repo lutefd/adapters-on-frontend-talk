96ba26f3038bdc0b32b294260acc18810e21fe62
Luis Dourado <lfaddourado@gmail.com>
2024-09-15 20:52:05 -0300

    feat(frontend): setup frontend with astro for multiple framework examples

---
+++ frontend/astro.config.mjs
@@ -0,0 +1,12 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+
+import tailwind from '@astrojs/tailwind';
+import svelte from '@astrojs/svelte';
+import react from '@astrojs/react';
+import vue from '@astrojs/vue';
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [tailwind(), svelte(), react(), vue()]
+});
