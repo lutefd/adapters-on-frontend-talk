75df152c3897c492fdc1798919b59f9feae95930
Luis Dourado <lfaddourado@gmail.com>
2024-09-15 20:40:34 -0300

    feat(server): add chi router with multiple api versions

---
+++ backend/cmd/api/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/Lutefd/adapters-on-frontend-talk/internal/controller"
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+)
+
+func main() {
+	r := chi.NewRouter()
+	r.Use(middleware.Logger)
+
+	userController := controller.NewUserController()
+	r.Route("/api", func(r chi.Router) {
+		r.Route("/v1", func(r chi.Router) {
+			r.Get("/users", userController.GetUsers)
+		})
+		r.Route("/v2", func(r chi.Router) {
+			r.Get("/users", userController.GetUsersAlternative)
+		})
+	})
+
+	log.Println("Server starting on port 8080")
+	log.Fatal(http.ListenAndServe(":8080", r))
+}
