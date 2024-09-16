acaab036d33e78ccec3a9cde82a28928fc53ddfe
Luis Dourado <lfaddourado@gmail.com>
2024-09-15 21:14:36 -0300

    feat(server): add cors config

--- backend/cmd/api/main.go
+++ backend/cmd/api/main.go
@@ -7,11 +7,20 @@ import (
 	"github.com/Lutefd/adapters-on-frontend-talk/internal/controller"
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
+	"github.com/go-chi/cors"
 )
 
 func main() {
 	r := chi.NewRouter()
 	r.Use(middleware.Logger)
+	r.Use(cors.Handler(cors.Options{
+		AllowedOrigins:   []string{"*"},
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
+		ExposedHeaders:   []string{"Link"},
+		AllowCredentials: false,
+		MaxAge:           300,
+	}))
 
 	userController := controller.NewUserController()
 	r.Route("/api", func(r chi.Router) {
